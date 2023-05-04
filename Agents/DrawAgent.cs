a15cc513d5a2588516a86aba6ad296d5df9195d1
soolek <soolek@gmail.com>
2023-05-04 22:57:34 +0200

    working commong tangents

--- Agents/DrawAgent.cs
+++ Agents/DrawAgent.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using MonoGame;
 using Model;
+using System.Collections;
 
 namespace Agents
 {
@@ -29,72 +30,77 @@ namespace Agents
                 {
                     var corner = state.Corners[i];
                     var prevCorner = i > 0 ? state.Corners[i - 1] : null;
-                    
+
                     // main
                     spriteBatch.DrawCircle(corner.Center.ToVector2(), corner.Radius, 50, Color.Green);
                     var dirVector = new Vector2((float)Math.Cos(corner.Direction), (float)Math.Sin(corner.Direction));
                     spriteBatch.DrawLine(corner.Center.ToVector2(), corner.Center.ToVector2() + dirVector * corner.Radius, Color.Green);
 
                     // tangents
+                    var colors = new[] { Color.Green, Color.Red, Color.GreenYellow, Color.OrangeRed };
                     if (prevCorner != null)
                     {
-                        var tangent = FindConnectingTangent(prevCorner, corner);
-                        spriteBatch.DrawLine(tangent.Item1, tangent.Item2, Color.Yellow);
+                        var tangents = FindConnectingTangent(prevCorner, corner);
+                        for (var j = 0; j < tangents.Count(); j++)
+                        {
+                            var tangent = tangents.ElementAt(j);
+                            spriteBatch.DrawLine(tangent.Item1, tangent.Item2, colors[j]);
+                        }
                     }
                 }
 
                 if (state.StartPosition != null && state.EndPosition != null)
                 {
                     spriteBatch.DrawLine(state.StartPosition.Value.ToVector2(), state.EndPosition.Value.ToVector2(), Color.Red);
-                    spriteBatch.DrawCircle(state.EndPosition.Value.ToVector2(),
-                        Vector2.Distance(state.StartPosition.Value.ToVector2(), state.EndPosition.Value.ToVector2()),
+                    spriteBatch.DrawCircle(
+                        state.EndPosition.Value.ToVector2(),
+                        Vector2.Distance(state.StartPosition.Value.ToVector2(),
+                        state.EndPosition.Value.ToVector2()),
                         50,
-                        Color.Red);
+                        Color.Red
+                    );
                 }
             }
             spriteBatch.End();
         }
 
-        // ChatGPT failed?
-        public static (Vector2,Vector2) FindConnectingTangent(Corner c1, Corner c2)
+        public static IEnumerable<(Vector2, Vector2)> FindConnectingTangent(Corner c1, Corner c2)
         {
-            var x1 = c1.Center.X;
-            var y1 = c1.Center.Y;
-            var r1 = c1.Radius;
-            var x2 = c2.Center.X;
-            var y2 = c2.Center.Y;
-            var r2 = c2.Radius;
+            // Calculate the distance between the two circle centers
+            float dx = c2.Center.X - c1.Center.X;
+            float dy = c2.Center.Y - c1.Center.Y;
+            float d = (float)Math.Sqrt(dx * dx + dy * dy);
 
-            // Calculate the distance between the centers of the circles
-            double d = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            // Check if the circles are coincident
+            if (d <= Math.Abs(c1.Radius - c2.Radius))
+            {
+                yield break;
+            }
 
-            // Calculate the angle between the line connecting the centers of the circles and the x-axis
-            double theta = Math.Atan2(y2 - y1, x2 - x1);
+            // Calculate the angle between the two centers
+            float angle = (float)Math.Atan2(dy, dx);
 
-            // Calculate the radius vectors of the circles
-            double r1x = r1 * Math.Cos(theta);
-            double r1y = r1 * Math.Sin(theta);
-            double r2x = r2 * Math.Cos(theta + Math.PI);
-            double r2y = r2 * Math.Sin(theta + Math.PI);
+            // Calculate the angle between the tangent line and the line connecting the two centers
+            float alpha = (float)Math.Acos((c1.Radius - c2.Radius) / d);
 
-            // Calculate the angle between the radius vector and the tangent
-            double alpha = Math.Acos((r2 - r1) / d);
+            // Calculate the points where the tangents intersect the circles
+            var p1 = new Vector2(c1.Center.X + c1.Radius * (float)Math.Cos(angle + alpha), c1.Center.Y + c1.Radius * (float)Math.Sin(angle + alpha));
+            var p2 = new Vector2(c1.Center.X + c1.Radius * (float)Math.Cos(angle - alpha), c1.Center.Y + c1.Radius * (float)Math.Sin(angle - alpha));
+            var p3 = new Vector2(c2.Center.X + c2.Radius * (float)Math.Cos(angle + alpha), c2.Center.Y + c2.Radius * (float)Math.Sin(angle + alpha));
+            var p4 = new Vector2(c2.Center.X + c2.Radius * (float)Math.Cos(angle - alpha), c2.Center.Y + c2.Radius * (float)Math.Sin(angle - alpha));
 
-            // Calculate the direction of the tangent
-            double dir = Math.Sign(r2 - r1);
+            yield return (p1, p3);
+            yield return (p2, p4);
 
-            // Calculate the coordinates of the point of tangency
-            double tx = x1 + (r1 + dir * r1 * Math.Cos(alpha)) * Math.Cos(theta);
-            double ty = y1 + (r1 + dir * r1 * Math.Cos(alpha)) * Math.Sin(theta);
+            // Calculate the points where the transverse tangents intersect the circles
+            float beta = (float)Math.Acos((c1.Radius + c2.Radius) / d);
+            var p5 = new Vector2(c1.Center.X + c1.Radius * (float)Math.Cos(angle + beta), c1.Center.Y + c1.Radius * (float)Math.Sin(angle + beta));
+            var p6 = new Vector2(c1.Center.X + c1.Radius * (float)Math.Cos(angle - beta), c1.Center.Y + c1.Radius * (float)Math.Sin(angle - beta));
+            var p7 = new Vector2(c2.Center.X + c2.Radius * (float)Math.Cos(angle + beta + Math.PI), c2.Center.Y + c2.Radius * (float)Math.Sin(angle + beta + Math.PI));
+            var p8 = new Vector2(c2.Center.X + c2.Radius * (float)Math.Cos(angle - beta + Math.PI), c2.Center.Y + c2.Radius * (float)Math.Sin(angle - beta + Math.PI));
 
-            // Calculate the endpoints of the tangent line segment
-            double l = Math.Sqrt(Math.Pow(r1 + dir * r1 * Math.Cos(alpha), 2) - Math.Pow(r1, 2));
-            double lx1 = tx + l * Math.Cos(theta + dir * Math.PI / 2);
-            double ly1 = ty + l * Math.Sin(theta + dir * Math.PI / 2);
-            double lx2 = tx + l * Math.Cos(theta - dir * Math.PI / 2);
-            double ly2 = ty + l * Math.Sin(theta - dir * Math.PI / 2);
-
-            return new(new Vector2((float)lx1, (float)ly1), new Vector2((float)lx2, (float)ly2));
+            yield return (p5, p7);
+            yield return (p6, p8);
         }
     }
 }
